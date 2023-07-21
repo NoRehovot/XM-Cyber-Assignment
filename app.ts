dd2f1c6ce5969a4d5536d677401f5f48761981a1
malina <99268466+NoRehovot@users.noreply.github.com>
2023-07-21 16:17:56 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -5,12 +5,11 @@ const searchWord: string = 'mouse';
 function SearchSuggestion(products: string[], searchWord: string) : string[][]{
     let resultProducts: string[];  // a temporary version of products to hold the user's search results
     let searchByLetters: string;  // temporary string to save the search word as it would appear in each stage of the user's typing
+    let finalResult: string[][] = [];  // will hold each of the search result arrays for every letter typed
 
     // sort products lexicographically
     products.sort();
-
-    let finalResult: string[][] = [];  // will hold each of the search result arrays for every letter typed
-
+    
     // go over the length of searchWord
     for (let i = 1; i <= searchWord.length; i++) {
         searchByLetters = searchWord.slice(0, i);  // slice the search word to get searchByLetters (see definition)
